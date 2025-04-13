a0cba42269b3bd93ae0e45e97fd520d7fdea029a
Alfasamec3D <navigatorprestigio7777@gmail.com>
2025-04-13 22:19:31 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/9C468886468862BE/media/programs/cyberprotect
-# Build directory: /mnt/9C468886468862BE/media/programs/cyberprotect/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/mnt/9C468886468862BE/media/programs/cyberprotect/build/test_run[1]_include.cmake")
-subdirs("_deps/googletest-build")
